ec48266d178d161789c7e10011e76370a6d83865
Fahim Agahi <fahim.agahi@gmail.com>
2022-08-28 21:00:43 +0430

    Feat: First commit

---
+++ fahim.agahi-finalproject/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header className='py-8 w-full h-full bg-white'> Site Header</header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
